5fddd754f56a6af98eaa0455d7fefb9f0dbd9cb5
Frank Wang Ma <frankwangma@gmail.com>
2021-12-10 00:26:47 +1300

    Clean up day 9

--- 9/first.js
+++ 9/first.js
@@ -4,10 +4,18 @@ let input = global
   .loadInput()
   .lines().map(i => i.replace('\r', '').split(''));
 
+  xPos = 0;
+  yPos = 0;
   input = input.map(line => line.map(height => {
     const object = {};
     object.Value = height;
     object.IsLow = true;
+    object.Position = [yPos, xPos];
+    xPos++;
+    if(xPos == input[0].length) {
+      xPos = 0;
+      yPos++
+    }
     return object;
   }))
 
@@ -16,41 +24,13 @@ let input = global
     var checkBottom = true;
     for(var j = 0; j < input[i].length; j++) {
       
-      var checkLeft = true;
-      var checkRight  = true;
-      if(i == 0) {
-        checkTop = false;
-      } else if (i == input.length - 1) {
-        checkBottom = false;
-      }
-      if(j == 0) {
-        checkLeft = false;
-      } else if(j == input[i].length - 1) {
-        checkRight = false;
-      }
-      
-      if (checkTop) {
-        if(input[i][j].Value >= input[i - 1][j].Value) {
-          input[i][j].IsLow = false;
-        }
-      }
-
-      if (checkBottom) {
-        if(input[i][j].Value >= input[i + 1][j].Value) {
-          input[i][j].IsLow = false;
-        }
-      }
-      if (checkLeft) {
-        if(input[i][j].Value >= input[i][j - 1].Value) {
-          input[i][j].IsLow = false;
-        }
-      }
-      if (checkRight) {
-        if(input[i][j].Value >= input[i][j + 1].Value) {
-          input[i][j].IsLow = false;
-        }
-      }
-      
+     var neighbours = findNeighbours(input[i][j]);
+     for(const neighbour of neighbours) {
+       if(input[i][j].Value >= neighbour.Value) {
+         input[i][j].IsLow = false;
+       }
+     }
+  
     }
   }
 
@@ -59,5 +39,44 @@ let input = global
   for(const obj of trueValues) {
     sum += (parseInt(obj.Value,10)) + 1;
   }
+
+  function findNeighbours(node) {
+    var neighbours = [];
+    var checkTop = true;
+    var checkBottom = true;
+    var checkLeft = true;
+    var checkRight  = true;
+  
+    var nodeY = node.Position[0];
+    var nodeX = node.Position[1];
+  
+    if(nodeY == 0) {
+      checkTop = false;
+    } else if (nodeY == input.length - 1) {
+      checkBottom = false;
+    }
+    if(nodeX == 0) {
+      checkLeft = false;
+    } else if(nodeX == input[nodeY].length - 1) {
+      checkRight = false;
+    }
+  
+    if (checkTop) {
+      neighbours.push(input[nodeY - 1][nodeX])
+    }
+  
+    if (checkBottom) {
+      neighbours.push(input[nodeY + 1][nodeX])
+    }
+    if (checkLeft) {
+      neighbours.push(input[nodeY][nodeX - 1])
+    }
+    if (checkRight) {
+      neighbours.push(input[nodeY][nodeX + 1])
+    }
+  
+    return neighbours;
+  }
+  
 
   console.log(sum);
